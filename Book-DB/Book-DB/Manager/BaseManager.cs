9e39b9a99457d98e524a5dbb3316d67082d0ab92
Erik <101416162+Eriknpy@users.noreply.github.com>
2023-02-22 01:46:19 +0100

    create : BaseManager

---
+++ Book-DB/Book-DB/Manager/BaseManager.cs
@@ -0,0 +1,54 @@
+﻿using Book_DB.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_DB.Manager
+{
+    internal abstract class BaseManager
+    {
+        protected readonly UserInterface _ui;
+        protected abstract string GetName();
+        protected abstract void List();
+        protected abstract void Add();
+        protected abstract void Edit();
+        public BaseManager(UserInterface ui)
+        {
+            _ui = ui;
+        }
+
+        public void Run()
+        {
+            _ui.Clear();
+            var running = true;
+            while (running)
+            {
+                _ui.PrintTitle(GetName());
+                _ui.PrintOption('l', "List");
+                _ui.PrintOption('a', "Add");
+                _ui.PrintOption('e', "Edit");
+                _ui.PrintOption('b', "Back to main menu");
+
+                switch (_ui.Choice(" laeb "))
+                {
+                    case 'l':
+                        List();
+                        break;
+                    case 'a':
+                        Add();
+                        break;
+                    case 'e':
+                        Edit();
+                        break;
+                    case 'b':
+                        running = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
