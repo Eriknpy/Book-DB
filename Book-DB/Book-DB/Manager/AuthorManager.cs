279ee817449ca4c73c9106936bb5848e0213341e
Erik <101416162+Eriknpy@users.noreply.github.com>
2023-02-22 01:46:49 +0100

    create : Author and Book managers

---
+++ Book-DB/Book-DB/Manager/AuthorManager.cs
@@ -0,0 +1,67 @@
+﻿using Book_DB.Model.Interfaces;
+using Book_DB.Model;
+using Book_DB.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_DB.Manager
+{
+    internal class AuthorManager : BaseManager
+    {
+        private readonly IAuthorDao _authorDao;
+
+        public AuthorManager(UserInterface ui, IAuthorDao authorDao)
+            : base(ui)
+        {
+            _authorDao = authorDao;
+        }
+
+        protected override void Add()
+        {
+            var firstName = _ui.ReadString("First name", "John");
+            var lastName = _ui.ReadString("Last name", "Doe");
+            var defaultDate = new DateOnly(1900, 5, 28);
+            var birthDate = _ui.ReadDate("Birth date", defaultDate);
+            _authorDao.Add(new Author(firstName, lastName, birthDate));
+        }
+
+        protected override void Edit()
+        {
+            List();
+            var id = _ui.ReadInt("Author ID", 0);
+            var author = _authorDao.Get(id);
+            if (author == null)
+            {
+                _ui.PrintLn("Author not found!");
+                return;
+            }
+
+            _ui.PrintLn(author);
+
+            var firstName = _ui.ReadString("First name", author.FirstName);
+            var lastName = _ui.ReadString("Last name", author.LastName);
+            var birthDate = _ui.ReadDate("Birth date", author.BirthDate);
+            author.FirstName = firstName;
+            author.LastName = lastName;
+            author.BirthDate = birthDate;
+            _authorDao.Update(author);
+        }
+
+        protected override string GetName()
+        {
+            return "Author Manager";
+        }
+
+        protected override void List()
+        {
+            _ui.PrintLn("Registered authors:");
+            foreach (var author in _authorDao.GetAll())
+            {
+                _ui.PrintLn(author);
+            }
+        }
+    }
+}
---
+++ Book-DB/Book-DB/Manager/BookManager.cs
@@ -0,0 +1,90 @@
+﻿using Book_DB.Model.Interfaces;
+using Book_DB.Model;
+using Book_DB.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_DB.Manager
+{
+    internal class BookManager : BaseManager
+    {
+        private readonly IBookDao _bookDao;
+        private readonly IAuthorDao _authorDao;
+        public BookManager(UserInterface ui, IBookDao bookDao, IAuthorDao authorDao)
+            : base(ui)
+        {
+            _bookDao = bookDao;
+            _authorDao = authorDao;
+        }
+
+        protected override void Add()
+        {
+            ListAuthors();
+            var authorId = _ui.ReadInt("Author ID", 0);
+            var title = _ui.ReadString("Title", "Z");
+
+            var author = _authorDao.Get(authorId);
+            if (author == null)
+            {
+                _ui.PrintLn("Author was not found!");
+                return;
+            }
+            var newBook = new Book(author, title);
+            _bookDao.Add(newBook);
+        }
+
+        protected override void Edit()
+        {
+            List();
+            var id = _ui.ReadInt(" Book ID", 0);
+            var book = _bookDao.Get(id);
+
+            if (book == null)
+            {
+                _ui.PrintLn("Book not found");
+                return;
+            }
+            _ui.PrintLn(book);
+            ListAuthors();
+
+            var authorId = _ui.ReadInt(" Author ID", book.Author.Id);
+            var title = _ui.ReadString("Title", book.Title);
+
+            var author = _authorDao.Get(authorId);
+            if (author == null)
+            {
+                _ui.PrintLn(" Author not found!");
+                return;
+            }
+            book.Author = author;
+            book.Title = title;
+            _bookDao.Update(book);
+        }
+
+        private void ListAuthors()
+        {
+            _ui.PrintLn("Registered authors:");
+            foreach (var item in _authorDao.GetAll())
+            {
+                _ui.PrintLn(item);
+            }
+        }
+
+        protected override string GetName()
+        {
+            return "Book Manager"; ;
+        }
+
+        protected override void List()
+        {
+            _ui.PrintLn("Registered books:");
+            foreach (var item in _bookDao.GetAll())
+            {
+                _ui.PrintLn(item);
+            }
+        }
+    }
+}
