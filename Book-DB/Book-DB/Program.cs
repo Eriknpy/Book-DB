45b6c1d7c9d4bcf5c4507b646ba8630f7611a5ca
Erik <101416162+Eriknpy@users.noreply.github.com>
2023-02-22 01:47:18 +0100

    add : BookDbManager

--- Book-DB/Book-DB/Program.cs
+++ Book-DB/Book-DB/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Book_DB
+﻿using Book_DB.Manager;
+using Book_DB.View;
+
+namespace Book_DB
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var ui = new UserInterface();
+            var bookDbManager = new BookDbManager(ui);
+            bookDbManager.Run();
         }
     }
 }
