8a05647faed71ffeb159ad83e9c556debf738f7a
Erik <101416162+Eriknpy@users.noreply.github.com>
2023-02-22 01:45:04 +0100

    create : Models
    + Book
    + Author

---
+++ Book-DB/Book-DB/Model/Author.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_DB.Model
+{
+    internal class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly BirthDate { get; set; }
+
+        public Author(string firstName, string lastName, DateOnly birthDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return new string($"{Id}, {FirstName}, {LastName}, {BirthDate: MM/dd/yyyy}");
+        }
+    }
+}
---
+++ Book-DB/Book-DB/Model/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_DB.Model
+{
+    internal class Book
+    {
+        public int Id { get; set; }
+        public Author Author { get; set; }
+        public string Title { get; set; }
+
+        public Book(Author author, string title)
+        {
+            Author = author;
+            Title = title;
+        }
+
+        public override string ToString()
+        {
+            return new string($"{Id}, {Title}, {Author.FirstName}, {Author.LastName}");
+        }
+    }
+}
